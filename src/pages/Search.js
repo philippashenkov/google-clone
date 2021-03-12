9be51caa6fea3d5afc4dac7d10321f7c62f0927f
Phil Pashenkov <reikoslv@gmail.com>
2021-03-12 06:03:52 +0300

    finish basic style layout

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Search.css';
 import MicIcon from '@material-ui/icons/Mic';
 import SearchIcon from '@material-ui/icons/Search';
+import {Button} from "@material-ui/core";
 
 function Search(props) {
     return (
@@ -10,6 +11,11 @@ function Search(props) {
                 <SearchIcon className='search__inputIcon' />
                 <input />
                 <MicIcon />
+            </div>
+
+            <div className='search__buttons'>
+                <Button variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
             </div>
         </div>
     )
